9464a5c468d200ad80028f701465c78c0599ec77
bloweyelashwish <blueyouthlol@gmail.com>
2022-09-16 20:52:52 +0300

    Form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
+    try {
+        const user = await loginService.login({ username, password })
+        setUser(user)
+        setUsername('')
+        setPassword('')
+    } catch (exception) {
+        console.log(exception)
+    }
   }
 
   useEffect(() => {
