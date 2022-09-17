c276f3b21ac02d28ab496d7a54306a9ec064f4a9
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-17 19:50:25 +0300

    sort blogs by likes count

--- src/App.js
+++ src/App.js
@@ -98,9 +98,13 @@ const App = () => {
                     />
                 </Togglable>
                 <hr/>
-                {blogs.map(blog =>
-                    <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-                )}
+                {blogs
+                    .sort((a, b) => b.likes - a.likes)
+                    .map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)
+                }
+                {/*{blogs.map(blog =>*/}
+                {/*    <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />*/}
+                {/*)}*/}
             </div>
         )
     }
