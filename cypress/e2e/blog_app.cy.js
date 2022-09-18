b06d17bc3fc43fab4d7ea463b4f73966e60862a0
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-18 17:57:13 +0300

    E2E  add blog test

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -14,15 +14,32 @@ describe('Blog app', function() {
     cy.contains('log in')
   })
 
-    it('success with correct creds', function() {
-        cy.get('#username').type('testusername')
-        cy.get('#password').type('test')
-        cy.get('#login-button').click()
-        cy.get('#logout-btn').click()
+    describe('Login', function () {
+        it('success with correct creds', function() {
+            cy.get('#username').type('testusername')
+            cy.get('#password').type('test')
+            cy.get('#login-button').click()
+            cy.get('#logout-btn').click()
+        })
+        it('fails with wrong creds', function() {
+            cy.get('#username').type('testusername')
+            cy.get('#password').type('test123')
+            cy.get('#login-button').click()
+        })
     })
-    it('fails with wrong creds', function() {
-        cy.get('#username').type('testusername')
-        cy.get('#password').type('test123')
-        cy.get('#login-button').click()
+
+    describe('when logged in', function() {
+        beforeEach(function() {
+            cy.login({ username: 'testusername', password: 'test' })
+        })
+
+        it('A blog can be created', function() {
+            cy.contains('new note').click()
+            cy.get('#input-title').type('title with cypress')
+            cy.get('#input-author').type('author with cypress')
+            cy.get('#input-url').type('url with cypress')
+            cy.contains('create').click()
+        })
     })
+
 })
